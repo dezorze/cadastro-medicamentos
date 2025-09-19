e4ea580666f85af39f114120881a31f4c90988ef
dezorze <lfdprogram@gmail.com>
2025-09-19 12:01:02 -0300

    testando o git

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
-
+const testando = 'é um teste no github. vou apagar!'
 export default function CadastroMedicamentos() {
   const [medicamentos, setMedicamentos] = useState([])
   const [nome, setNome] = useState('')
